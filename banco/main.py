c2dca85b723f81ae421407c9a306766abd4f85c9
Roberto <161033388+Hubierto@users.noreply.github.com>
2024-09-24 14:11:42 +0000

    ok

---
+++ banco/main.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("cls || clear")
+
+#Instanciando classe
+if __name__ == "__main__":
+    os.system("pytest")
---
+++ banco/tests/test_conta.py
@@ -0,0 +1,25 @@
+import pytest
+from banco.models.conta import Conta
+
+#testes de cobertura
+
+@pytest.fixture
+def conta_valida():
+    conta1 = Conta(222,333)
+    return conta1
+
+def test_numero_conta_valida(conta_valida):
+    assert conta_valida._numero_conta == 222
+
+def test_agencia_valida(conta_valida):
+    assert conta_valida._agencia == 333
+
+def test_depositar_valor_positivo(conta_valida):
+    conta_valida.depositar(100)       
+    assert conta_valida._saldo == 100
+
+def test_saldo_inicial_zero(conta_valida):
+    assert conta_valida._saldo == 0  
+
+def depositar_valor_negativo_saldo_zero(conta_valida):
+    assert conta_valida._saldo == -3      
